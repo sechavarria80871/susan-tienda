1fc03c4adae5c34b91a11373ba48982173dc31d7
sechavarria80871 <sechavarria80871@ufide.ac.cr>
2025-10-13 23:18:26 -0600

    Clase 1
    Generación de index y otras páginas html, así como la definición de los estilos

---
+++ susan-tienda/src/main/java/tiendaTech/com/tienda/TiendaTechApplication.java
@@ -0,0 +1,13 @@
+package tiendaTech.com.tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaTechApplication.class, args);
+	}
+
+}
---
+++ susan-tienda/src/test/java/tiendaTech/com/tienda/TiendaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package tiendaTech.com.tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
